4d8564bc50b5c931c621d5f5734e7fe0fd8deed0
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-06-01 16:50:40 -0300

    Formatação

--- src/stores/data/UserStore.ts
+++ src/stores/data/UserStore.ts
@@ -1,49 +1,41 @@
-import type { UserModel } from "@/corelib/models/UserModel";
-import UserService from "@/corelib/services/UserService";
-import { defineStore } from "pinia";
+import type { UserModel } from '@/corelib/models/UserModel';
+import UserService from '@/corelib/services/UserService';
+import { defineStore } from 'pinia';
 
 interface InternalStoreStateType {
-    userInfo: UserModel;
+  userInfo: UserModel;
 }
 
-export const useUserStore = defineStore('UserStore',{
-    state: (): InternalStoreStateType => ({  
-        userInfo: {              
-            userId: null,
-            userName: null,
-            userPassword: null,
-            userEmail: null
-        }
-    }),
-    getters: {
-        getUserInfo: (state: InternalStoreStateType): UserModel =>
-            state.userInfo
+export const useUserStore = defineStore('UserStore', {
+  state: (): InternalStoreStateType => ({
+    userInfo: {
+      userId: null,
+      userName: null,
+      userPassword: null,
+      userEmail: null
+    }
+  }),
+  getters: {
+    getUserInfo: (state: InternalStoreStateType): UserModel => state.userInfo
     //    UserInfo: (state: UserModel) => state,
     //    UserId: (state) => state.userId,
     //    UserName: (state) => state.userName,
     //    UserPassword: (state) => state.userPassword,
     //    UserEmail: (state) => state.userEmail
+  },
+  actions: {
+    atribuirUser(value: UserModel): void {
+      this.userInfo = value;
     },
-    actions: {
-        atribuirUser(value: UserModel): void {
-            this.userInfo = value;
-        },
-        
-        getUser: async function (
-            email: string,
-            password: string
-        ): 
-            Promise<void> {
-            const service = new UserService('url/local');
 
-            try {
-                this.atribuirUser(await service.getUser(
-                    email, 
-                    password
-                ));
-            } catch {
-                return;
-            }
-        }
+    getUser: async function (email: string, password: string): Promise<void> {
+      const service = new UserService('url/local');
+
+      try {
+        this.atribuirUser(await service.getUser(email, password));
+      } catch {
+        return;
+      }
     }
+  }
 });
